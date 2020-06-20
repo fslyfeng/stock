dcaef1f560cb107ef0fa85cdcde11a174f0e8b97
netren <netren@netrendeiMac.local>
2020-06-20 18:00:16 +0800

    使用swagger和装饰器编写接口文档

--- server-api/apps/admin-api/src/main.ts
+++ server-api/apps/admin-api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('stock库存-后台管理API')
+    .setDescription('提供后台管理界面的服务端接口')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
-  console.log(`http://localhost:3000`);
+  console.log(`http://localhost:3000/api-docs`);
 }
 bootstrap();
--- server-api/libs/db/src/models/user.model.ts
+++ server-api/libs/db/src/models/user.model.ts
@@ -1,9 +1,17 @@
-import { prop } from '@typegoose/typegoose';
+import { prop, modelOptions } from '@typegoose/typegoose';
+import { ApiProperty } from '@nestjs/swagger';
 
+@modelOptions({
+  schemaOptions: {
+    timestamps: true,
+  },
+})
 export class User {
+  @ApiProperty({ description: '用户名', example: 'user' })
   @prop()
   username: string;
 
+  @ApiProperty({ description: '密码', example: 'pass' })
   @prop()
   password: string;
 }
